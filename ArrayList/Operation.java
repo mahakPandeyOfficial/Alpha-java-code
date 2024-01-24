ee318a295172b356b36669ae52f07ca02ea870b0
Mahak Pandey <116765334+mahakPandeyOfficial@users.noreply.github.com>
2024-01-24 23:06:32 +0530

    adding Code of operation perform on ArrayList.

---
+++ ArrayList/Operation.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Operation{
+    public static void main(String[] args){
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        //Adding elements to the list 
+        list1.add(1);
+        list1.add(3);
+        list1.add(5);
+        list1.add(7);
+        list1.add(9);
+        list1.add(11);
+
+        System.out.println("The list 1 is after adding element : " + list1);
+
+        // Get element by providing index number
+        // int element = list1.get(5);
+        // System.out.println( "The element at 5 th index : "+ element);
+
+        //Remove the element from arrayList
+       // list1.remove(2);
+       // System.out.println("The list 1 after removing from index 2 : "+  list1);
+
+        //set element at given index
+        list1.set(5, 13);
+        System.out.println("The list after setting value 13 at 5th index : " + list1);
+
+        //Contains element is the operation for cheacking existence of the element in the list
+        System.out.println(list1.contains(9));
+        System.out.println(list1.contains(11));
+    }
+}
